98de8d79eba3f2eead57b68bb8a517ecafa18cbc
compassionate-scope-9333 <lexus2424@gmail.com>
2019-04-24 17:22:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,11 +18,8 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(cheese)
-  i = 0
-  cheese_types =["cheddar", "gouda", "camembert"]
-  while i < cheese_types.length 
- return  cheese_types[i]
-   if array.include?(cheese_types[i])
-     i+= 1
-   end
- end
+cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find {|a| a == "cheddar"}
+  
+end
+end
